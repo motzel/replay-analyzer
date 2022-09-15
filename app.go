280eb74c7d8c628234058f5b117d848dcd6bc969
motzel <motzel@o2.pl>
2022-09-15 17:12:27 +0200

    feat: create default Wails app

---
+++ app.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/wailsapp/wails/v2/pkg/runtime"
+)
+
+// App struct
+type App struct {
+	ctx context.Context
+}
+
+// NewApp creates a new App application struct
+func NewApp() *App {
+	return &App{}
+}
+
+// startup is called when the app starts. The context is saved
+// so we can call the runtime methods
+func (a *App) startup(ctx context.Context) {
+	a.ctx = ctx
+}
+
+func (b *App) beforeClose(ctx context.Context) (prevent bool) {
+	dialog, err := runtime.MessageDialog(ctx, runtime.MessageDialogOptions{
+		Title:   "Quit?",
+		Message: "Are you sure you want to quit?",
+		Type:    runtime.QuestionDialog,
+	})
+
+	if err != nil {
+		return false
+	}
+
+	return dialog != "Yes"
+}
+
+// Greet returns a greeting for the given name
+func (a *App) Greet(name string) string {
+	return fmt.Sprintf("Hello %s, It's show time!", name)
+}
