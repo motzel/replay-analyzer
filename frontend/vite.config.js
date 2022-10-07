f36f3a96687cff48d929e9fafbf66fe9ed6e7965
motzel <motzel@o2.pl>
2022-10-07 15:20:31 +0200

    feat: add copying frontend assets dist directory during the build

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,21 @@
 import {defineConfig} from 'vite'
 import {svelte} from '@sveltejs/vite-plugin-svelte'
+import {viteStaticCopy} from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-  optimizeDeps: {
-    exclude: ['tinro']
-  },
+    plugins: [
+        svelte(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: 'assets/*',
+                    dest: 'assets'
+                }
+            ]
+        })
+    ],
+    optimizeDeps: {
+        exclude: ['tinro']
+    },
 })
